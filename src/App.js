712bf837b4c77929822f2994898762a67b8d1aaa
Marcio Martins <mmarciom1martins635@gmail.com>
2023-09-06 18:04:49 -0300

    .

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Fragment, useState } from "react";
 import Carrinho from "./componentes/botaoCarrinhoDeCompras";
 import PageCompras from "./componentes/Carrinho";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {FcIphone} from "react-icons/fc";
 
 
 function App() {
@@ -171,7 +172,7 @@ function App() {
     },
 
   ]);
-  const [carrinho, setCarrinho] = useState([])
+  const [carrinho, setCarrinho] = useState([]);
   
 
  
@@ -245,7 +246,7 @@ function App() {
 
 
       </main>
-      <p className="pc"> Responsivo até width: 800px</p>
+      <p className="pc"> Responsivo até width: 800px <FcIphone/></p>
     </Fragment>
   );
 }
